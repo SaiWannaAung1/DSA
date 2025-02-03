51bb7e39671426a61d1d7b445ecc0e12de2bfb25
SaiWunnaAung <40501792@live.napier.ac.uk>
2025-02-03 17:17:00 +0000

    linear search

--- src/searching/LinearSearch.java
+++ src/searching/LinearSearch.java
@@ -1,5 +1,22 @@
 package searching;
 
-public class LinearSearch {
+import javax.swing.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class LinearSearch {
+    public static void main(String[] args) {
+        new LinearSearch().impl("Martin");
+    }
+    public  void impl(String searchKey){
+        ArrayList<String> personList = new ArrayList<>(
+                Arrays.asList("Robin","Lutin","Martin","Cake","Brownie")
+        );
+        for (String person: personList) {
+            if (person.equals(searchKey)){
+                JOptionPane.showMessageDialog(null,"Find "+person+" at index "+ personList.indexOf(person));
+            }
+        }
+    }
 }
