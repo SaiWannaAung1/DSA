a3833b9f55afa76427ce6d985e37f33591d4850b
SaiWunnaAung <40501792@live.napier.ac.uk>
2025-02-04 01:28:09 +0000

    update some code in LinearSearch & implement binary search

---
+++ src/searching/BinarySearch.java
@@ -0,0 +1,37 @@
+package searching;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        ArrayList<String> personList = new ArrayList<>(
+                Arrays.asList("Robin","Lutin","Martin","Cake","Brownie")
+        );
+        Collections.sort(personList);
+        boolean result = new BinarySearch().impl("Cake",personList);
+        JOptionPane.showMessageDialog(null, result);
+
+    }
+
+    boolean impl(String searchText, ArrayList<String> personList) {
+        int left = 0, right = personList.size() - 1;
+
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            if (searchText.equals(personList.get(middle))){
+                return true;
+            }
+
+            if (personList.get(middle).compareTo(searchText) < 0){
+                left = middle + 1;
+            }else {
+                right = middle - 1;
+            }
+        }
+
+        return false;
+    }
+}
--- src/searching/LinearSearch.java
+++ src/searching/LinearSearch.java
@@ -1,22 +1,26 @@
 package searching;
 
 import javax.swing.*;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class LinearSearch {
+
     public static void main(String[] args) {
-        new LinearSearch().impl("Martin");
-    }
-    public  void impl(String searchKey){
         ArrayList<String> personList = new ArrayList<>(
                 Arrays.asList("Robin","Lutin","Martin","Cake","Brownie")
         );
+        new LinearSearch().impl("Martin",personList);
+    }
+
+    public  void impl(String searchKey, ArrayList<String> personList){
+
         for (String person: personList) {
             if (person.equals(searchKey)){
                 JOptionPane.showMessageDialog(null,"Find "+person+" at index "+ personList.indexOf(person));
             }
         }
     }
+
+
 }
